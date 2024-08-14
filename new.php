9eb22f2f9ecb46dc4f18bd9e9a7e2cc8b39d2cf3
Your Name <you@example.com>
2024-08-14 12:05:30 +0600

    bdcalling it frontend

---
+++ new.php
@@ -0,0 +1,176 @@
+
+<div class="real_estate_display_grid">
+  <div class="real_estate_title">
+    <h3 class="heading-style-h3">Webflow Website |  Charming Independent Plot for Sale in Chennai</h3>
+    <div class="location_text">
+      <img src="https://assets-global.website-files.com/6620c39e0f73c5b63cb850e8/662202c0c58468fa14215e7d_fi-bs-marker.svg" loading="lazy" alt="">
+      <div class="text-style-muted">Thalapathi Galli, Chennai.</div>
+    </div>
+  </div>
+  <div class="divider_line"></div>
+  <div class="real_estate_details">
+    <div id="w-node-a8d19551-4778-0097-8db9-f138cf4659eb-0e2a2261" class="div_left">
+      <div class="real_estate_images_wrapper">
+        <a href="#" class="lightbox-link w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
+          <img src="https://assets-global.website-files.com/6621eca8425e124cf274a6dc/6624054b330fd69fdd1fb197_property%20image.webp" loading="lazy" alt="" class="image_cms">
+          <script type="application/json" class="w-json">
+            {
+              "items": [
+                {
+                  "url": "https://assets-global.website-files.com/6621eca8425e124cf274a6dc/6624054b330fd69fdd1fb197_property%20image.webp",
+                  "type": "image"
+                }
+              ],
+              "group": "property images"
+            }
+          </script>
+        </a>
+        <div id="w-node-e0b9ab9e-ba01-9f78-32aa-4f862c54692d-0e2a2261" class="w-layout-layout wf-layout-layout">
+          <div id="w-node-e0b9ab9e-ba01-9f78-32aa-4f862c54692e-0e2a2261" class="w-layout-cell">
+            <a href="#" class="w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
+              <img src="https://assets-global.website-files.com/6621eca8425e124cf274a6dc/6638600f1692352962152902_home%20decor%20dummy%204.webp" loading="lazy" alt="">
+              <script type="application/json" class="w-json">
+                {
+                  "items": [
+                    {
+                      "url": "https://assets-global.website-files.com/6621eca8425e124cf274a6dc/6638600f1692352962152902_home%20decor%20dummy%204.webp",
+                      "type": "image"
+                    }
+                  ],
+                  "group": "property images"
+                }
+              </script>
+            </a>
+          </div>
+          <div id="w-node-e0b9ab9e-ba01-9f78-32aa-4f862c54692f-0e2a2261" class="w-layout-cell">
+            <a href="#" class="w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
+              <img src="https://assets-global.website-files.com/6621eca8425e124cf274a6dc/66385eed512cabfb3beae9e8_home%20decor%20dummy%202%20(1).webp" loading="lazy" alt="">
+              <script type="application/json" class="w-json">
+                {
+                  "items": [
+                    {
+                      "url": "https://assets-global.website-files.com/6621eca8425e124cf274a6dc/66385eed512cabfb3beae9e8_home%20decor%20dummy%202%20(1).webp",
+                      "type": "image"
+                    }
+                  ],
+                  "group": "property images"
+                }
+              </script>
+            </a>
+          </div>
+          <div id="w-node-_78708b83-0c8a-a357-1a0f-23518fc6a1f9-0e2a2261" class="w-layout-cell">
+            <a href="#" class="is_overlay_set w-inline-block w-lightbox" aria-label="open lightbox" aria-haspopup="dialog">
+              <img src="https://assets-global.website-files.com/6621eca8425e124cf274a6dc/6624054b330fd69fdd1fb197_property%20image.webp" loading="lazy" alt="">
+              <div class="image_overlay_text">View More</div>
+              <script type="application/json" class="w-json">
+                {
+                  "items": [
+                    {
+                      "url": "https://assets-global.website-files.com/6621eca8425e124cf274a6dc/6624054b330fd69fdd1fb197_property%20image.webp",
+                      "type": "image"
+                    }
+                  ],
+                  "group": "property images"
+                }
+              </script>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div id="w-node-f247bf92-5974-45da-a9e0-cbf20b9cc665-0e2a2261" class="real_estate_info">
+        <h5 class="heading-style-h5">Overview</h5>
+        <div class="spacer_div"></div>
+        <div class="visual_info_wrapper">
+          <div id="w-node-_448dbcbe-6f2e-58aa-6a8e-f930dcdca463-0e2a2261" class="icon_cell">
+            <img src="https://assets-global.website-files.com/6620c39e0f73c5b63cb850e8/66221367d19859ef89be5c1e_bed.svg" loading="lazy" alt="">
+            <div class="text-size-small">26</div>
+            <div class="text-size-small">Rooms</div>
+          </div>
+          <div id="w-node-_5c4b485f-591b-92b5-2674-9fcfb8c0713a-0e2a2261" class="icon_cell">
+            <img src="https://assets-global.website-files.com/6620c39e0f73c5b63cb850e8/662213c63766dd981431c7b2_bathtub.svg" loading="lazy" alt="">
+            <div class="text-size-small">24</div>
+            <div class="text-size-small">Bathrooms</div>
+          </div>
+          <div id="w-node-e7801214-bbad-f105-2ef2-eb4cdd428723-0e2a2261" class="icon_cell">
+            <img src="https://assets-global.website-files.com/6620c39e0f73c5b63cb850e8/662213d35c4638ae2e4cca6e_balcony.svg" loading="lazy" alt="">
+            <div class="text-size-small">Justice-facing</div>
+          </div>
+          <div id="w-node-e4a7afc8-8777-eb5f-c69a-dc328bc225b7-0e2a2261" class="icon_cell">
+            <img src="https://assets-global.website-files.com/6620c39e0f73c5b63cb850e8/662213e2c93f6d2e96ebf492_shelves.svg" loading="lazy" alt="">
+            <div class="text-size-small">$500k</div>
+          </div>
+        </div>
+        <div class="spacer_div"></div>
+        <div class="text_info_wrapper">
+          <div class="info_text_cell">
+            <h6 class="heading-style-h6 text-style-muted">Carpet Area</h6>
+            <div>2600 sq ft</div>
+          </div>
+          <div class="info_text_cell">
+            <h6 class="heading-style-h6 text-style-muted">Build Date</h6>
+            <div>March 28, 2025</div>
+          </div>
+          <div class="info_text_cell">
+            <h6 class="heading-style-h6 text-style-muted">Property Type</h6>
+            <div>velit-laborum-consectetur-asperiores</div>
+          </div>
+          <div class="info_text_cell">
+            <h6 class="heading-style-h6 text-style-muted">Flat Direction</h6>
+            <div>Justice-facing</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="div_right">
+    <div id="w-node-f1459dd4-678d-39ad-29ec-e03ab36e9ce4-0e2a2261" class="real_estate_price_details">
+        <h5 class="heading-style-h5 text-style-muted">Property Value</h5>
+        <div class="spacer_div"></div>
+        <h4 class="heading-style-h4">$500k</h4>
+        <div class="spacer_div"></div>
+        <div>Your bid cannot be more than 10% of minimum property value.</div>
+        <div class="spacer_div"></div>
+        <div>
+            <h6 class="heading-style-h6">Min</h6>
+            <div class="range_box_text">$500k</div>
+        </div>
+        <div>
+            <h6 class="heading-style-h6">Max</h6>
+            <div class="range_box_text">$500k</div>
+        </div>
+        <div class="spacer_div"></div>
+        <div class="range_slider_component">
+            <div class="range_slider">
+                <div class="fs-rangeslider_form w-form">
+                    <form id="email-form" name="email-form" data-name="Email Form" method="get" data-wf-page-id="6621eca8f5ae2cf20e2a2261" data-wf-element-id="f491019e-e658-84e6-ed19-576ffb3a214d" aria-label="Email Form">
+                        <div fs-rangeslider-formatdisplay="true" fs-rangeslider-element="wrapper" fs-rangeslider-min="0" fs-rangeslider-max="100000" class="fs-rangeslider_wrapper">
+                            <div fs-rangeslider-element="track" class="fs-rangeslider_track-2">
+                                <div fs-rangeslider-element="handle" class="fs-rangeslider_handle"></div>
+                                <div fs-rangeslider-element="handle" class="fs-rangeslider_handle is-pink"></div>
+                                <div fs-rangeslider-element="fill" class="fs-rangeslider_fill-2"></div>
+                            </div>
+                            <input class="fs-rangeslider_input w-input" maxlength="256" name="From-3" fs-cmsfilter-field="price" data-name="From 3" fs-cmsfilter-range="from" placeholder="From" type="text" id="From-3">
+                            <input class="fs-rangeslider_input w-input" maxlength="256" name="To-3" fs-cmsfilter-field="price" data-name="To 3" fs-cmsfilter-range="to" placeholder="To" type="text" id="To-3">
+                            <div class="fs-range_values text-size-small">
+                                <div>$<span fs-rangeslider-element="display-value">0</span></div>
+                                <div>$<span fs-rangeslider-element="display-value">0</span></div>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="w-form-done" tabindex="-1" role="region" aria-label="Email Form success">
+                        <div>Thank you! Your submission has been received!</div>
+                    </div>
+                    <div class="w-form-fail" tabindex="-1" role="region" aria-label="Email Form failure">
+                        <div>Oops! Something went wrong while submitting the form.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="#" class="button_the_second w-inline-block">
+            <div class="button_text text-size-medium">Bid Property</div>
+        </a>
+        </div>
+        <div id="w-node-_60411cda-b5b8-2fde-73fb-36e10e181997-0e2a2261" class="real_estate_location_map">
+            <img src="https://assets-global.website-files.com/6621eca8425e124cf274a6dc/66221e7625082c857e6b6602_Rectangle%2027.webp" loading="lazy" alt="">
+        </div>
+    </div>
+</div>
